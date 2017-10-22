6bd35ce13d830067a310a3f06160e93a458bb707
Xantes Fabela <xantesfabela@Xantess-iMac.local>
2017-10-22 14:50:28 -0500

    a few mods

---
+++ window-shopper02/window-shopper02/Views/CurrencyTxtField.swift
@@ -0,0 +1,15 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper02
+//
+//  Created by Xantes Fabela on 10/22/17.
+//  Copyright © 2017 Xantes Fabela. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+
+
+}
