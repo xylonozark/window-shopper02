6b0d1ddcf26c5ad22faa1755b6a589e54f5cd664
Xantes Fabela <xantesfabela@Xantess-iMac.local>
2017-10-22 13:35:58 -0500

    first commit

---
+++ window-shopper02/window-shopper02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper02
+//
+//  Created by Xantes Fabela on 10/22/17.
+//  Copyright © 2017 Xantes Fabela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
